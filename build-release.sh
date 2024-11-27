e13c9dae4f732cc094fc0f85fc09a79f1ffdebfa
ibuetler <ivan.buetler@hacking-lab.com>
2024-11-27 12:13:33 +0100

    update

--- build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -t hackinglab/alpine-nodejs:$1.0 -t hackinglab/alpine-nodejs:$1 -t hackinglab/alpine-nodejs:latest -f Dockerfile .
-
-docker push hackinglab/alpine-nodejs
-docker push hackinglab/alpine-nodejs:$1
-docker push hackinglab/alpine-nodejs:$1.0
-
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-nodejs:3.2.0 -t hackinglab/alpine-nodejs:3.2 -t hackinglab/alpine-nodejs:latest -f Dockerfile .
-
